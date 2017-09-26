eccd6cfa0c794147f15986a7d67b9b5c64b341d4
russel <russel@rlvillacarlos>
2017-09-26 14:12:09 +0800

    Updates

--- COSC111/src/edu/java/cosc111/samples/A.java
+++ COSC111/src/edu/java/cosc111/samples/A.java
@@ -15,7 +15,7 @@ public class A {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello World");
     }
     
 }
